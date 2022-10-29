0ed2362d444f0ba941a181a821d4dff570559a77
PGowland91 <99564438+PGowland@users.noreply.github.com>
2022-10-29 16:21:51 -0700

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,24 +1,27 @@
 from turtle import Turtle
 
+with open("high_score.txt") as high_score_file:
+    high_score_saved = int(high_score_file.read())
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = high_score_saved
         self.penup()
         self.hideturtle()
         self.goto(0, 260)
         self.color("white")
-        self.write(f"Score = {self.score}    High Score = {self.high_score}", font=("arial", 20, "bold"), align="center")
-
+        self.write(f"Score = {self.score}    High Score = {self.high_score}", font=("arial", 20, "bold"),
+                   align="center")
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score = {self.score}    High Score = {self.high_score}", font=("arial", 20, "bold"), align="center")
-
+        self.write(f"Score = {self.score}    High Score = {self.high_score}", font=("arial", 20, "bold"),
+                   align="center")
 
     def game_over(self):
         self.clear()
@@ -28,10 +31,12 @@ class Scoreboard(Turtle):
         self.write(f"Your final score was: {self.score}", font=("arial", 20, "bold"), align="center")
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("high_score.txt", mode="w") as self.high_score_file:
+                self.high_score_file.write(str(self.high_score))
 
     def update_scoreboard(self):
         self.clear()
         self.goto(0, 260)
         self.score = 0
-        self.write(f"Score = {self.score}    High Score = {self.high_score}", font=("arial", 20, "bold"), align="center")
-
+        self.write(f"Score = {self.score}    High Score = {self.high_score}", font=("arial", 20, "bold"),
+                   align="center")
